1c812897514744902f4a7bd383a1665bfeeeb296
Stellexd <benjaminingacio.polanco@gmail.com>
2023-09-19 22:27:40 -0300

    commit funcional añadiendo clase main

---
+++ src/redesSocialesWaza/instagram.java
@@ -0,0 +1,2 @@
+package redesSocialesWaza;public class instagram {
+}
--- src/redesSocialesWaza/persona.java
+++ src/redesSocialesWaza/persona.java
@@ -4,16 +4,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class persona {
+    int opc;
     String usuario, contrasena;
 
     public persona(String usuario, String contrasena) {
         this.usuario = usuario;
         this.contrasena = contrasena;
+        this.opc= opc;
     }
 
     public static void main(String[] args) {
         System.out.println("Elige tu opción: \n 1. Crear usuario \n 2. Ingresar a Twitter \n 3. Ingresar a Instagram\nEs necesario crear un usuario para seguir con el programa");
         Scanner sc = new Scanner(System.in);
         ArrayList<String> cuenta = new ArrayList<>();
+        int opc = sc.nextInt();
+        if (opc ==1){
+            System.out.println("Ingresa tu usuario: ");
+                String usuario = sc.nextLine();
+            System.out.println("Ingresa tu contraseña: ");
+                String contrasena = sc.nextLine();
+            persona persona = new persona(usuario, contrasena);
+            cuenta.add(usuario);
+            cuenta.add(contrasena);
+            System.out.println(cuenta);
+        }
     }
 }
